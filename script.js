40c0f6cc97dde2a7edcf68ba8788ce9325ea3a40
Pulkit saini <51844439+sainipulkit12@users.noreply.github.com>
2023-08-14 02:32:57 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('nav-items');
+const close = document.getElementById('close');
+
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+
+
+// ##########################################################
+// sproduct
+
+
